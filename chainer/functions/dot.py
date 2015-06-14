c6a09ebcfc09a8ada0271656ce9495029444d5d4
muupan <muupan@gmail.com>
2015-06-14 16:14:29 +0900

    Dot now supports GPU computation.

--- chainer/functions/dot.py
+++ chainer/functions/dot.py
@@ -1,19 +1,72 @@
-from chainer import Function
+from chainer import cuda, Function
 import numpy
+
+def _as_mat(x):
+    # vectors are considered as column vectors
+    return x.reshape(1, (len(x))) if len(x.shape) == 1 else x
+
+def _as_trans_op(trans):
+    return 't' if trans else 'n'
+
+def _dot_cpu_notransout(x, y, transa=False, transb=False):
+    x = _as_mat(x)
+    y = _as_mat(y)
+    if transa:
+        x = x.T
+    if transb:
+        y = y.T
+    return numpy.dot(x, y)
+
+def _dot_cpu(x, y, transa=False, transb=False, transout=False):
+    if transout:
+        # (X Y)^T = Y^T X^T
+        return _dot_cpu_notransout(y, x, transa=not transb, transb=not transa)
+    else:
+        return _dot_cpu_notransout(x, y, transa=transa, transb=transb)
+
+def _dot_gpu_notransout(x, y, transa=False, transb=False):
+    x = _as_mat(x)
+    y = _as_mat(y)
+    with cuda.using_cumisc():
+        return cuda.culinalg.dot(x, y,
+                transa=_as_trans_op(transa),
+                transb=_as_trans_op(transb))
+
+def _dot_gpu(x, y, transa=False, transb=False, transout=False):
+    if transout:
+        # (X Y)^T = Y^T X^T
+        return _dot_gpu_notransout(y, x, transa=not transb, transb=not transa)
+    else:
+        return _dot_gpu_notransout(x, y, transa=transa, transb=transb)
 
 class Dot(Function):
     """Dot product: matrix multiplication between 2D arrays or inner product
     between 1D arrays."""
+
+    def __init__(self, transa=False, transb=False):
+        self.transa = transa
+        self.transb = transb
+
     def forward_cpu(self, x):
         assert len(x[0].shape) == len(x[1].shape)
-        return numpy.dot(x[0], x[1]),
+        return _dot_cpu(x[0], x[1], transa=self.transa, transb=self.transb),
+
+    def forward_gpu(self, x):
+        assert len(x[0].shape) == len(x[1].shape)
+        return _dot_gpu(x[0], x[1], transa=self.transa, transb=self.transb),
 
     def backward_cpu(self, x, gy):
-        gx0 = numpy.dot(gy[0], x[1].T).reshape(x[0].shape)
-        gx1 = numpy.dot(x[0].T, gy[0]).reshape(x[1].shape)
+        gx0 = _dot_cpu(gy[0], x[1], transb=not self.transb, transout=self.transa).reshape(x[0].shape)
+        gx1 = _dot_cpu(x[0], gy[0], transa=not self.transa, transout=self.transb).reshape(x[1].shape)
         return gx0, gx1
 
-def dot(x, y):
+    def backward_gpu(self, x, gy):
+        with cuda.using_cumisc():
+            gx0 = _dot_gpu(gy[0], x[1], transb=not self.transb, transout=self.transa).reshape(x[0].shape)
+            gx1 = _dot_gpu(x[0], gy[0], transa=not self.transa, transout=self.transb).reshape(x[1].shape)
+            return gx0, gx1
+
+def dot(x, y, transa=False, transb=False):
     """Compute dot product: matrix multiplication between 2D arrays or inner
     product between 1D arrays."""
-    return Dot()(x,y)
+    return Dot(transa=transa, transb=transb)(x, y)
--- tests/functions_tests/test_dot.py
+++ tests/functions_tests/test_dot.py
@@ -8,30 +8,30 @@ import chainer.functions as F
 cuda.init()
 
 class _TestDot(TestCase):
-    def check_forward(self, op, x1_data, x2_data):
+    def check_forward(self, x1_data, x2_data):
         x1 = Variable(x1_data)
         x2 = Variable(x2_data)
-        y = op(x1, x2)
+        y = self.op(x1, x2)
         if isinstance(y.data, cuda.GPUArray):
             self.assertTrue(hasattr(y.data.gpudata, 'device'))
-        assert_allclose(numpy.dot(self.x1, self.x2), y.data)
+        assert_allclose(self.forward_answer, y.data)
 
-    def forward_cpu(self, op):
-        self.check_forward(op, self.x1, self.x2)
+    def forward_cpu(self):
+        self.check_forward(self.x1, self.x2)
 
     def test_dot_forward_cpu(self):
-        self.forward_cpu(lambda x, y: F.dot(x, y))
+        self.forward_cpu()
 
-    def forward_gpu(self, op):
-        self.check_forward(op, to_gpu(self.x1), to_gpu(self.x2))
+    def forward_gpu(self):
+        self.check_forward(to_gpu(self.x1), to_gpu(self.x2))
 
     def test_dot_forward_gpu(self):
-        self.forward_gpu(lambda x, y: F.dot(x, y))
+        self.forward_gpu()
 
-    def check_backward(self, op, x1_data, x2_data, y_grad, atol):
+    def check_backward(self, x1_data, x2_data, y_grad, atol):
         x1 = Variable(x1_data)
         x2 = Variable(x2_data)
-        y = op(x1, x2)
+        y = self.op(x1, x2)
         y.grad = y_grad
         y.backward()
 
@@ -41,27 +41,62 @@ class _TestDot(TestCase):
         assert_allclose(gx1, x1.grad, atol=atol)
         assert_allclose(gx2, x2.grad, atol=atol)
 
-    def backward_cpu(self, op, atol=1e-5):
-        self.check_backward(op, self.x1, self.x2, self.gy, atol)
+    def backward_cpu(self, atol=1e-2):
+        self.check_backward(self.x1, self.x2, self.gy, atol)
 
     def test_dot_backward_cpu(self):
-        self.backward_cpu(lambda x, y: F.dot(x, y))
+        self.backward_cpu()
 
-    def backward_gpu(self, op, atol=1e-5):
-        self.check_backward(op, to_gpu(self.x1), to_gpu(self.x2), to_gpu(self.gy), atol)
+    def backward_gpu(self, atol=1e-2):
+        self.check_backward(to_gpu(self.x1), to_gpu(self.x2), to_gpu(self.gy), atol)
 
     def test_dot_backward_gpu(self):
-        self.backward_gpu(lambda x, y: F.dot(x, y))
+        self.backward_gpu()
 
-class TestDotMatrix(_TestDot):
+class TestDotMatrixMatrix(_TestDot):
     def setUp(self):
-        self.x1 = numpy.random.uniform(.5, 1, (2, 1)).astype(numpy.float32)
-        self.x2 = numpy.random.uniform(.5, 1, (1, 3)).astype(numpy.float32)
-        self.gy = numpy.random.uniform(-1, 1, (2, 3)).astype(numpy.float32)
+        self.x1 = numpy.random.uniform(.5, 1, (2, 5)).astype(numpy.float32)
+        self.x2 = numpy.random.uniform(.5, 1, (5,10)).astype(numpy.float32)
+        self.gy = numpy.random.uniform(-1, 1, (2,10)).astype(numpy.float32)
+        self.op = lambda x, y: F.dot(x, y)
+        self.forward_answer = numpy.dot(self.x1, self.x2)
 
-class TestDotVector(_TestDot):
+class TestDotMatrixTMatrix(_TestDot):
     def setUp(self):
-        self.x1 = numpy.random.uniform(.5, 1, (3,)).astype(numpy.float32)
-        self.x2 = numpy.random.uniform(.5, 1, (3,)).astype(numpy.float32)
-        self.gy = numpy.random.uniform(-1, 1, ()).astype(numpy.float32)
+        self.x1 = numpy.random.uniform(.5, 1, (5, 2)).astype(numpy.float32)
+        self.x2 = numpy.random.uniform(.5, 1, (5,10)).astype(numpy.float32)
+        self.gy = numpy.random.uniform(-1, 1, (2,10)).astype(numpy.float32)
+        self.op = lambda x, y: F.dot(x, y, transa=True)
+        self.forward_answer = numpy.dot(self.x1.T, self.x2)
 
+class TestDotMatrixMatrixT(_TestDot):
+    def setUp(self):
+        self.x1 = numpy.random.uniform(.5, 1, (2, 5)).astype(numpy.float32)
+        self.x2 = numpy.random.uniform(.5, 1, (10,5)).astype(numpy.float32)
+        self.gy = numpy.random.uniform(-1, 1, (2,10)).astype(numpy.float32)
+        self.op = lambda x, y: F.dot(x, y, transb=True)
+        self.forward_answer = numpy.dot(self.x1, self.x2.T)
+
+class TestDotMatrixTMatrixT(_TestDot):
+    def setUp(self):
+        self.x1 = numpy.random.uniform(.5, 1, (5, 2)).astype(numpy.float32)
+        self.x2 = numpy.random.uniform(.5, 1, (10,5)).astype(numpy.float32)
+        self.gy = numpy.random.uniform(-1, 1, (2,10)).astype(numpy.float32)
+        self.op = lambda x, y: F.dot(x, y, transa=True, transb=True)
+        self.forward_answer = numpy.dot(self.x1.T, self.x2.T)
+
+class TestDotVectorVectorT(_TestDot):
+    def setUp(self):
+        self.x1 = numpy.random.uniform(.5, 1, (5,)).astype(numpy.float32)
+        self.x2 = numpy.random.uniform(.5, 1, (5,)).astype(numpy.float32)
+        self.gy = numpy.random.uniform(-1, 1, (1, 1)).astype(numpy.float32)
+        self.op = lambda x, y: F.dot(x, y, transb=True)
+        self.forward_answer = numpy.dot(self.x1, self.x2)
+
+class TestDotVectorTVector(_TestDot):
+    def setUp(self):
+        self.x1 = numpy.random.uniform(.5, 1, (5,)).astype(numpy.float32)
+        self.x2 = numpy.random.uniform(.5, 1, (5,)).astype(numpy.float32)
+        self.gy = numpy.random.uniform(-1, 1, (5, 5)).astype(numpy.float32)
+        self.op = lambda x, y: F.dot(x, y, transa=True)
+        self.forward_answer = numpy.dot(self.x1.reshape(5, 1), self.x2.reshape(1, 5))
