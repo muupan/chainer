09b8e08116214777bfaeb319f934b11bdf4a0426
muupan <muupan@gmail.com>
2015-06-14 19:01:29 +0900

    Implement dot and batchdot functions.

--- chainer/functions/dot.py
+++ chainer/functions/dot.py
@@ -2,7 +2,7 @@ from chainer import cuda, Function
 import numpy
 
 def _as_mat(x):
-    # vectors are considered as column vectors
+    # 1-D arrays are considered as column vectors
     return x.reshape(1, (len(x))) if len(x.shape) == 1 else x
 
 def _as_trans_op(trans):
@@ -24,25 +24,24 @@ def _dot_cpu(x, y, transa=False, transb=False, transout=False):
     else:
         return _dot_cpu_notransout(x, y, transa=transa, transb=transb)
 
-def _dot_gpu_notransout(x, y, transa=False, transb=False):
+def _dot_gpu_notransout(x, y, transa=False, transb=False, out=None):
     x = _as_mat(x)
     y = _as_mat(y)
     with cuda.using_cumisc():
         return cuda.culinalg.dot(x, y,
                 transa=_as_trans_op(transa),
-                transb=_as_trans_op(transb))
+                transb=_as_trans_op(transb),
+                out=out)
 
-def _dot_gpu(x, y, transa=False, transb=False, transout=False):
+def _dot_gpu(x, y, transa=False, transb=False, transout=False, out=None):
     if transout:
         # (X Y)^T = Y^T X^T
-        return _dot_gpu_notransout(y, x, transa=not transb, transb=not transa)
+        return _dot_gpu_notransout(y, x,
+                transa=not transb, transb=not transa, out=out)
     else:
-        return _dot_gpu_notransout(x, y, transa=transa, transb=transb)
+        return _dot_gpu_notransout(x, y, transa=transa, transb=transb, out=out)
 
 class Dot(Function):
-    """Dot product: matrix multiplication between 2D arrays or inner product
-    between 1D arrays."""
-
     def __init__(self, transa=False, transb=False):
         self.transa = transa
         self.transb = transb
@@ -56,17 +55,116 @@ class Dot(Function):
         return _dot_gpu(x[0], x[1], transa=self.transa, transb=self.transb),
 
     def backward_cpu(self, x, gy):
-        gx0 = _dot_cpu(gy[0], x[1], transb=not self.transb, transout=self.transa).reshape(x[0].shape)
-        gx1 = _dot_cpu(x[0], gy[0], transa=not self.transa, transout=self.transb).reshape(x[1].shape)
+        gx0 = _dot_cpu(
+                gy[0], x[1], transb=not self.transb, transout=self.transa
+                ).reshape(x[0].shape)
+        gx1 = _dot_cpu(
+                x[0], gy[0], transa=not self.transa, transout=self.transb
+                ).reshape(x[1].shape)
         return gx0, gx1
 
     def backward_gpu(self, x, gy):
         with cuda.using_cumisc():
-            gx0 = _dot_gpu(gy[0], x[1], transb=not self.transb, transout=self.transa).reshape(x[0].shape)
-            gx1 = _dot_gpu(x[0], gy[0], transa=not self.transa, transout=self.transb).reshape(x[1].shape)
+            gx0 = _dot_gpu(
+                    gy[0], x[1], transb=not self.transb, transout=self.transa
+                    ).reshape(x[0].shape)
+            gx1 = _dot_gpu(
+                    x[0], gy[0], transa=not self.transa, transout=self.transb
+                    ).reshape(x[1].shape)
             return gx0, gx1
 
 def dot(x, y, transa=False, transb=False):
-    """Compute dot product: matrix multiplication between 2D arrays or inner
-    product between 1D arrays."""
+    """Compute dot product of two arrays.
+
+    Args:
+        x, y: Variables of 2-D or 1-D arrays.
+            A 2-D array with shape (N, M) is considered as a NxM matrix.
+            A 1-D array with shape (N,) is considered as a 1xN matrix.
+        transa (bool): If true, transpose x.
+        transb (bool): If true, transpose y.
+
+    Returns:
+        ~chainer.Variable: Dot product of x and y as a 2-D array
+    """
     return Dot(transa=transa, transb=transb)(x, y)
+
+class BatchDot(Function):
+    def __init__(self, transa=False, transb=False):
+        self.transa = transa
+        self.transb = transb
+
+    def _output_shape(self, a, b):
+        batch_size = a.shape[0]
+        a_mat_shape = _as_mat(a[0]).shape
+        b_mat_shape = _as_mat(b[0]).shape
+        m = a_mat_shape[1] if self.transa else a_mat_shape[0]
+        n = b_mat_shape[0] if self.transb else b_mat_shape[1]
+        return (batch_size, m, n)
+
+    def forward_cpu(self, x):
+        x0, x1 = x
+        assert x0.shape[0] == x1.shape[0]
+        batch_size = x0.shape[0]
+        shape = self._output_shape(x0, x1)
+        ret = numpy.empty(shape)
+        for i in xrange(batch_size):
+            ret[i] = _dot_cpu(
+                    x0[i], x1[i], transa=self.transa, transb=self.transb)
+        return ret,
+
+    def backward_cpu(self, x, gy):
+        x0, x1 = x
+        batch_size = x0.shape[0]
+        gx0 = numpy.empty(x0.shape)
+        gx1 = numpy.empty(x1.shape)
+        for i in xrange(batch_size):
+            gx0[i] = _dot_cpu(gy[0][i], x1[i],
+                    transb=not self.transb, transout=self.transa
+                    ).reshape(x0[0].shape)
+            gx1[i] = _dot_cpu(x0[i], gy[0][i],
+                    transa=not self.transa, transout=self.transb
+                    ).reshape(x1[0].shape)
+        return gx0, gx1
+
+    def forward_gpu(self, x):
+        x0, x1 = x
+        assert x0.shape[0] == x1.shape[0]
+        batch_size = x0.shape[0]
+        shape = self._output_shape(x0, x1)
+        ret = cuda.empty(shape)
+        # TODO(muupan) batch computation for more efficiency
+        for i in xrange(batch_size):
+             _dot_gpu(x0[i], x1[i],
+                     transa=self.transa, transb=self.transb, out=ret[i])
+        return ret,
+
+    def backward_gpu(self, x, gy):
+        x0, x1 = x
+        batch_size = x0.shape[0]
+        gx0 = cuda.empty((batch_size,) + _as_mat(x0[0]).shape)
+        gx1 = cuda.empty((batch_size,) + _as_mat(x1[0]).shape)
+        # TODO(muupan) batch computation for more efficiency
+        for i in xrange(batch_size):
+            _dot_gpu(gy[0][i], x1[i],
+                    transb=not self.transb, transout=self.transa, out=gx0[i])
+            _dot_gpu(x0[i], gy[0][i],
+                    transa=not self.transa, transout=self.transb, out=gx1[i])
+        gx0 = gx0.reshape(x0.shape)
+        gx1 = gx1.reshape(x1.shape)
+        return gx0, gx1
+
+def batchdot(x, y, transa=False, transb=False):
+    """Compute dot product of two arrays in a batch manner.
+
+    Args:
+        x, y: Variables of 3-D or 2-D arrays.
+            A 3-D array of shape (B, N, M) is considered as B NxM matrices.
+            A 2-D array of shape (B, N,) is considered as B 1xN matrices.
+        transa (bool): If true, transpose each matrix in x.
+        transb (bool): If true, transpose each matrix in y.
+
+    Returns:
+        ~chainer.Variable: Batch of dot products of two matrices from x and y in
+            a 3-D array.
+    """
+    return BatchDot(transa=transa, transb=transb)(x, y)
--- tests/functions_tests/test_dot.py
+++ tests/functions_tests/test_dot.py
@@ -48,55 +48,137 @@ class _TestDot(TestCase):
         self.backward_cpu()
 
     def backward_gpu(self, atol=1e-2):
-        self.check_backward(to_gpu(self.x1), to_gpu(self.x2), to_gpu(self.gy), atol)
+        self.check_backward(
+                to_gpu(self.x1), to_gpu(self.x2), to_gpu(self.gy), atol)
 
     def test_dot_backward_gpu(self):
         self.backward_gpu()
 
+m = 2
+k = 5
+n = 10
+
 class TestDotMatrixMatrix(_TestDot):
     def setUp(self):
-        self.x1 = numpy.random.uniform(.5, 1, (2, 5)).astype(numpy.float32)
-        self.x2 = numpy.random.uniform(.5, 1, (5,10)).astype(numpy.float32)
-        self.gy = numpy.random.uniform(-1, 1, (2,10)).astype(numpy.float32)
+        self.x1 = numpy.random.uniform(.5, 1, (m, k)).astype(numpy.float32)
+        self.x2 = numpy.random.uniform(.5, 1, (k, n)).astype(numpy.float32)
+        self.gy = numpy.random.uniform(-1, 1, (m, n)).astype(numpy.float32)
         self.op = lambda x, y: F.dot(x, y)
         self.forward_answer = numpy.dot(self.x1, self.x2)
 
 class TestDotMatrixTMatrix(_TestDot):
     def setUp(self):
-        self.x1 = numpy.random.uniform(.5, 1, (5, 2)).astype(numpy.float32)
-        self.x2 = numpy.random.uniform(.5, 1, (5,10)).astype(numpy.float32)
-        self.gy = numpy.random.uniform(-1, 1, (2,10)).astype(numpy.float32)
+        self.x1 = numpy.random.uniform(.5, 1, (k, m)).astype(numpy.float32)
+        self.x2 = numpy.random.uniform(.5, 1, (k, n)).astype(numpy.float32)
+        self.gy = numpy.random.uniform(-1, 1, (m, n)).astype(numpy.float32)
         self.op = lambda x, y: F.dot(x, y, transa=True)
         self.forward_answer = numpy.dot(self.x1.T, self.x2)
 
 class TestDotMatrixMatrixT(_TestDot):
     def setUp(self):
-        self.x1 = numpy.random.uniform(.5, 1, (2, 5)).astype(numpy.float32)
-        self.x2 = numpy.random.uniform(.5, 1, (10,5)).astype(numpy.float32)
-        self.gy = numpy.random.uniform(-1, 1, (2,10)).astype(numpy.float32)
+        self.x1 = numpy.random.uniform(.5, 1, (m, k)).astype(numpy.float32)
+        self.x2 = numpy.random.uniform(.5, 1, (n, k)).astype(numpy.float32)
+        self.gy = numpy.random.uniform(-1, 1, (m, n)).astype(numpy.float32)
         self.op = lambda x, y: F.dot(x, y, transb=True)
         self.forward_answer = numpy.dot(self.x1, self.x2.T)
 
 class TestDotMatrixTMatrixT(_TestDot):
     def setUp(self):
-        self.x1 = numpy.random.uniform(.5, 1, (5, 2)).astype(numpy.float32)
-        self.x2 = numpy.random.uniform(.5, 1, (10,5)).astype(numpy.float32)
-        self.gy = numpy.random.uniform(-1, 1, (2,10)).astype(numpy.float32)
+        self.x1 = numpy.random.uniform(.5, 1, (k, m)).astype(numpy.float32)
+        self.x2 = numpy.random.uniform(.5, 1, (n, k)).astype(numpy.float32)
+        self.gy = numpy.random.uniform(-1, 1, (m, n)).astype(numpy.float32)
         self.op = lambda x, y: F.dot(x, y, transa=True, transb=True)
         self.forward_answer = numpy.dot(self.x1.T, self.x2.T)
 
 class TestDotVectorVectorT(_TestDot):
     def setUp(self):
-        self.x1 = numpy.random.uniform(.5, 1, (5,)).astype(numpy.float32)
-        self.x2 = numpy.random.uniform(.5, 1, (5,)).astype(numpy.float32)
+        self.x1 = numpy.random.uniform(.5, 1, (m,)).astype(numpy.float32)
+        self.x2 = numpy.random.uniform(.5, 1, (m,)).astype(numpy.float32)
         self.gy = numpy.random.uniform(-1, 1, (1, 1)).astype(numpy.float32)
         self.op = lambda x, y: F.dot(x, y, transb=True)
-        self.forward_answer = numpy.dot(self.x1, self.x2)
+        self.forward_answer = numpy.dot(self.x1, self.x2).reshape(1, 1)
 
 class TestDotVectorTVector(_TestDot):
     def setUp(self):
-        self.x1 = numpy.random.uniform(.5, 1, (5,)).astype(numpy.float32)
-        self.x2 = numpy.random.uniform(.5, 1, (5,)).astype(numpy.float32)
-        self.gy = numpy.random.uniform(-1, 1, (5, 5)).astype(numpy.float32)
+        self.x1 = numpy.random.uniform(.5, 1, (m,)).astype(numpy.float32)
+        self.x2 = numpy.random.uniform(.5, 1, (m,)).astype(numpy.float32)
+        self.gy = numpy.random.uniform(-1, 1, (m, m)).astype(numpy.float32)
         self.op = lambda x, y: F.dot(x, y, transa=True)
-        self.forward_answer = numpy.dot(self.x1.reshape(5, 1), self.x2.reshape(1, 5))
+        self.forward_answer = numpy.dot(
+                self.x1.reshape(m, 1), self.x2.reshape(1, m))
+
+batch_size = 10
+
+class TestBatchDotMatrixMatrix(_TestDot):
+    def setUp(self):
+        self.x1 = numpy.random.uniform(
+                .5, 1, (batch_size, m, k)).astype(numpy.float32)
+        self.x2 = numpy.random.uniform(
+                .5, 1, (batch_size, k, n)).astype(numpy.float32)
+        self.gy = numpy.random.uniform(
+                -1, 1, (batch_size, m, n)).astype(numpy.float32)
+        self.op = lambda x, y: F.batchdot(x, y)
+        self.forward_answer = numpy.array([
+            numpy.dot(self.x1[i], self.x2[i]) for i in xrange(batch_size)])
+
+class TestBatchDotMatrixTMatrix(_TestDot):
+    def setUp(self):
+        self.x1 = numpy.random.uniform(
+                .5, 1, (batch_size, k, m)).astype(numpy.float32)
+        self.x2 = numpy.random.uniform(
+                .5, 1, (batch_size, k, n)).astype(numpy.float32)
+        self.gy = numpy.random.uniform(
+                -1, 1, (batch_size, m, n)).astype(numpy.float32)
+        self.op = lambda x, y: F.batchdot(x, y, transa=True)
+        self.forward_answer = numpy.array([
+            numpy.dot(self.x1[i].T, self.x2[i]) for i in xrange(batch_size)])
+
+class TestBatchDotMatrixMatrixT(_TestDot):
+    def setUp(self):
+        self.x1 = numpy.random.uniform(
+                .5, 1, (batch_size, m, k)).astype(numpy.float32)
+        self.x2 = numpy.random.uniform(
+                .5, 1, (batch_size, n, k)).astype(numpy.float32)
+        self.gy = numpy.random.uniform(
+                -1, 1, (batch_size, m, n)).astype(numpy.float32)
+        self.op = lambda x, y: F.batchdot(x, y, transb=True)
+        self.forward_answer = numpy.array([
+            numpy.dot(self.x1[i], self.x2[i].T) for i in xrange(batch_size)])
+
+class TestBatchDotMatrixTMatrixT(_TestDot):
+    def setUp(self):
+        self.x1 = numpy.random.uniform(
+                .5, 1, (batch_size, k, m)).astype(numpy.float32)
+        self.x2 = numpy.random.uniform(
+                .5, 1, (batch_size, n, k)).astype(numpy.float32)
+        self.gy = numpy.random.uniform(
+                -1, 1, (batch_size, m, n)).astype(numpy.float32)
+        self.op = lambda x, y: F.batchdot(x, y, transa=True, transb=True)
+        self.forward_answer = numpy.array([
+            numpy.dot(self.x1[i].T, self.x2[i].T) for i in xrange(batch_size)])
+
+class TestBatchDotVectorVectorT(_TestDot):
+    def setUp(self):
+        self.x1 = numpy.random.uniform(
+                .5, 1, (batch_size, m,)).astype(numpy.float32)
+        self.x2 = numpy.random.uniform(
+                .5, 1, (batch_size, m,)).astype(numpy.float32)
+        self.gy = numpy.random.uniform(
+                -1, 1, (batch_size, 1, 1)).astype(numpy.float32)
+        self.op = lambda x, y: F.batchdot(x, y, transb=True)
+        self.forward_answer = numpy.array(
+                [numpy.dot(self.x1[i], self.x2[i]) for i in xrange(batch_size)]
+                ).reshape(batch_size, 1, 1)
+
+class TestBatchDotVectorTVector(_TestDot):
+    def setUp(self):
+        self.x1 = numpy.random.uniform(
+                .5, 1, (batch_size, m,)).astype(numpy.float32)
+        self.x2 = numpy.random.uniform(
+                .5, 1, (batch_size, m,)).astype(numpy.float32)
+        self.gy = numpy.random.uniform(
+                -1, 1, (batch_size, m, m)).astype(numpy.float32)
+        self.op = lambda x, y: F.batchdot(x, y, transa=True)
+        self.forward_answer = numpy.array(
+                [numpy.dot(self.x1[i].reshape(m, 1),
+                    self.x2[i].reshape(1, m)) for i in xrange(batch_size)])
